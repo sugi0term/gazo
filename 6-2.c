1940f971c3528ae5888996f3f1bdc78d2ea860e1
Koki Sugiyama <post@max.fam.cx>
2016-11-03 22:01:52 +0900

    第６回

---
+++ 6-2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "iip.h"
+
+void
+mosaicImg(int);
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s src dest [option ...]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	getData(argv[1]);
+	rgbToYbr();
+	mosaicImg(atoi(argv[3]));
+	ybrToRgb();
+	putData(argv[2]);
+
+	return 0;
+}
+
+void
+mosaicImg(int blockSize)
+{
+	int i, j, m, n;
+	int sum, ave;
+
+	for (i = 0; i < ptr->biWidth; i += blockSize) {
+		for (j = 0; j < ptr->biHeight; j += blockSize) {
+			sum = 0;
+			for (m = i; m < i + blockSize; m++) {
+				for (n = j; n < j + blockSize; n++) {
+					sum += imgin[0][m][n];
+				}
+			}
+			ave = (int)(sum / (blockSize * blockSize));
+			for (m = i; m < i + blockSize; m++) {
+				for (n = j; n < j + blockSize; n++) {
+					imgout[0][m][n] = ave;
+					imgout[1][m][n] = 128;
+					imgout[2][m][n] = 128;
+				}
+			}
+		}
+	}
+}
---
+++ 6-3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "iip.h"
+
+void
+quantizeImg(int);
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s src dest [option ...]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	getData(argv[1]);
+	rgbToYbr();
+	quantizeImg(2);
+	ybrToRgb();
+	putData(argv[2]);
+
+	return 0;
+}
+
+void
+quantizeImg(int level)
+{
+	int i, j, k;
+	int lowerLimit, upperLimit;
+	int increment;
+
+	increment = 256 / level;
+	lowerLimit = 0;
+	upperLimit = increment;
+	for (k = 1; k <= level; k++) {
+		for (i = 0; i < ptr->biWidth; i++) {
+			for (j = 0; j < ptr->biHeight; j++) {
+				if (lowerLimit <= imgin[0][i][j] && imgin[0][i][j] < upperLimit) {
+					imgout[0][i][j] = upperLimit - increment;
+					imgout[1][i][j] = 128;
+					imgout[2][i][j] = 128;
+				}
+			}
+		}
+		lowerLimit += increment;
+		upperLimit += increment;
+	}
+}
